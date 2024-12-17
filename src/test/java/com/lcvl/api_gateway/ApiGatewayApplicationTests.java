122d17393eb283a6f466bafc95278ca17ba152d4
LuiscVillarrealL <luiscvillarreall@gmail.com>
2024-12-17 13:02:08 +0000

    circuit breaker

--- src/test/java/com/lcvl/api_gateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/lcvl/api_gateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,10 @@ package com.lcvl.api_gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.lcvl.gateway.ApiGatewayApplication;
+
+
+@SpringBootTest(classes = ApiGatewayApplication.class)
 class ApiGatewayApplicationTests {
 
 	@Test
